7d13ecd3400d1b6a6cb3990c69ab9fb5b3d31299
Denis Otkidach <denis.otkidach@gmail.com>
2020-12-18 19:24:04 +0300

    Refactor DrainTee, add push() method

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use slog::Drain;
+use slog::{Drain, Fuse};
 use slog_async::Async;
 use slog_gelf::Gelf;
 
-const LOG_BUFFER_SIZE: usize = 128;
+struct DrainTee(Vec<Fuse<Async>>);
 
-#[derive(Debug)]
-struct DrainMux<D>(Vec<D>);
+impl DrainTee {
+    pub fn default() -> Self {
+        Self(vec![])
+    }
 
-impl<D> DrainMux<D> {
-    pub fn new(d: Vec<D>) -> Self {
-        Self(d)
+    pub fn push<D>(&mut self, drain: D)
+    where
+        D: Drain<Err = slog::Never, Ok = ()> + Send + 'static,
+    {
+        self.0.push(Async::default(drain).fuse());
     }
 }
 
-impl<D: Drain> Drain for DrainMux<D> {
+impl Drain for DrainTee {
     type Ok = ();
-    type Err = D::Err;
+    type Err = slog::Never;
 
     fn log(
         &self,
@@ -32,30 +36,22 @@ fn main() -> anyhow::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
     std::env::set_var("GRAYLOG_URL", "localhost:12201");
 
+    let mut drain_tee = DrainTee::default();
+
     let decorator = slog_term::TermDecorator::new().stderr().build();
     let term_drain = slog_term::FullFormat::new(decorator).build().fuse();
+    drain_tee.push(term_drain);
 
     let graylog_url = std::env::var("GRAYLOG_URL")?;
     let host = hostname::get()?;
     let gelf_drain = Gelf::new(host.to_str().unwrap(), &graylog_url)?.fuse();
+    drain_tee.push(gelf_drain);
 
-    let drain_mux = DrainMux::new(vec![
-        Async::new(term_drain)
-            .chan_size(LOG_BUFFER_SIZE)
-            .build()
-            .fuse(),
-        Async::new(gelf_drain)
-            .chan_size(LOG_BUFFER_SIZE)
-            .build()
-            .fuse(),
-    ])
-    .fuse();
-    let logger = slog::Logger::root(drain_mux, slog::o!());
+    let logger = slog::Logger::root(drain_tee, slog::o!());
     let log_guard = slog_scope::set_global_logger(logger);
-    // slog::error!(logger, "Hello, slog!");
-    slog_scope::error!("Hello, slog_scope/drain_mux!");
-    log::error!("Hello, log!");
-    println!("Hello, world!");
+
+    slog_scope::error!("Hello, slog_scope!");
+
     std::mem::drop(log_guard);
     Ok(())
 }
