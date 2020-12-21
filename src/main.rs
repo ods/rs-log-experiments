4d5b69276cb68f1c28b901c6945b2a798006f083
Denis Otkidach <denis.otkidach@gmail.com>
2020-12-21 12:59:42 +0300

    Move version stamping into build.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use lazy_static::lazy_static;
-use regex::Regex;
 use slog::{Drain, Fuse, Level};
 use slog_async::Async;
 use slog_gelf::Gelf;
-use std::string::String;
 
 struct DrainTee(Vec<Fuse<Async>>);
 
@@ -49,43 +46,7 @@ impl Drain for DrainTee {
     }
 }
 
-lazy_static! {
-    // The resulting format is `major.minor.patch[+N+hash][+dirty]`.  The parts
-    // are:
-    //   * release (major.minor.patch)
-    //   * number of commits `N` on top of released version in development
-    //     version with commit hash abbreviation `hash`
-    //   * `dirty` marker for local uncommitted changes
-    //
-    // The release and revision come from annotated tag matching "*.*.*"
-    // pattern.  It falls back on `hash[+dirty]` when no release tag yet.
-    //
-    // `--match='*.*.*'` to take into account release tags only (and only
-    //      annotated, not lightweight)
-    // `--always` is for fallback while there were no releases yet
-    // `--first-parent` discards merge with revision tag after release (beware
-    //      that this might become undesired if we switch to git-flow)
-    // `--dirty` adds "-dirty" suffix when there are uncommited changes
-    //
-    // "-" are replaced with "+" since RPM doesn't allow dashes in version
-    // ("." is not used to avoid collision with revision).
-    // "g" (stands for "git") before hash is removed to avoid
-    // misinterpretation.
-    static ref APP_VERSION: String = Regex::new("-(?P<n_commits>[0-9]+)-g")
-        .unwrap()
-        .replace(
-            git_version::git_version!(
-                args = [
-                    "--always",
-                    "--match=*.*.*",
-                    "--first-parent",
-                    "--dirty"
-                ]
-            ),
-            "+$n_commits+",
-        )
-        .to_string();
-}
+const APP_VERSION: &str = env!("APP_VERSION");
 
 fn main() -> anyhow::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
