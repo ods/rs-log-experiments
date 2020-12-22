663bba133387114f0a873966798d3b13a70ab9e3
Denis Otkidach <denis.otkidach@gmail.com>
2020-12-22 08:56:49 +0300

    Make graylog/sentry optional

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use slog::Drain;
+use std::string::String;
+
+use slog::Drain; // Needed for `filter_level()` and `fuse()`
 
 use logging::DrainTee;
 
+fn get_var(name: &str) -> Option<String> {
+    std::env::var_os(name)
+        .map(|value| value.into_string().unwrap())
+        .filter(String::is_empty)
+}
+
 fn main() -> anyhow::Result<()> {
+    // Pretend these vars came from outside
     std::env::set_var("RUST_BACKTRACE", "1");
     std::env::set_var("ENVIRONMENT", "dev");
     std::env::set_var("GRAYLOG_URL", "localhost:12201");
@@ -11,17 +20,23 @@ fn main() -> anyhow::Result<()> {
         "http://185b7a7e069f4ef0983c2467e79683b1@localhost:9001/1",
     );
 
-    let environment = std::env::var("ENVIRONMENT")?;
-    let graylog_url = std::env::var("GRAYLOG_URL")?;
-    let sentry_url = std::env::var("SENTRY_URL")?;
-    let drain_tee = DrainTee::new(env!("APP_VERSION"), &environment)
-        .term()?
-        .graylog(&graylog_url)?
-        .sentry(&sentry_url)?;
-
+    // Normal application flow starts here
+    let environment =
+        get_var("ENVIRONMENT").unwrap_or_else(|| "unknown".into());
+    let mut drain_tee =
+        DrainTee::new(env!("APP_VERSION"), &environment).term()?;
+    if let Some(graylog_url) = get_var("GRAYLOG_URL") {
+        drain_tee = drain_tee.graylog(&graylog_url)?
+    }
+    if let Some(sentry_url) = get_var("SENTRY_URL") {
+        drain_tee = drain_tee.sentry(&sentry_url)?;
+    }
     let logger = slog::Logger::root(
         drain_tee.filter_level(slog::Level::Info).fuse(),
-        slog::o!("version" => env!("APP_VERSION")),
+        slog::o!(
+            "version" => env!("APP_VERSION"),
+            "environment" => environment,
+        ),
     );
     let log_guard = slog_scope::set_global_logger(logger);
 
